d09f72fcb4412aabb3232e179aee24f6e3fd6cd8
krell11 <11bobin.kiryusha@gmail.com>
2023-11-10 01:14:16 +0300

    script for backend + utils

---
+++ video/json_parser.py
@@ -0,0 +1,19 @@
+import json
+import re
+
+def parse_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def link_constructor(data):
+    return f"rtsp://{data['login']}:{data['password']}@{data['ip']}"
+
+
+def extract_id(file_path):
+    match = re.search(r'(video|stream)-(\d+)', file_path)
+    if match:
+        return int(match.group(2))
+    else:
+        return None
--- video/real_time_obj_detection.py
+++ video/real_time_obj_detection.py
@@ -1,8 +1,48 @@
 from video_stream import VideoStream
-from ultralytics import YOLO
+import argparse
+import os
+from json_parser import parse_json, link_constructor, extract_id
+import datetime
+import cv2
+from models.sift.sift import check_and_add_image
 
-weights_path = "ml_/airflow/best.pt"
+
+def check_folder(input_folder, output_folder):
+    processed_file = set()
+    while True:
+        for filename in os.listdir(input_folder):
+            if os.path.isfile(os.path.join(input_folder, filename)) and filename not in processed_file:
+                if filename.endswith('.json'):
+                    processed_file.add(filename)
+                    data = parse_json(filename)
+                    file_id = extract_id(filename)
+                    for i, detected_object in enumerate(VideoStream(link_constructor(data)).rtd()):
+                        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                        filename = f'{output_folder}/{file_id}/{file_id}_{i}'
+                        if check_and_add_image(filename, 'ml_/models/sift/data/images') is True:
+                            cv2.imwrite(filename, detected_object)
+                            cv2.imwrite('ml_/models/sift/data/images'+filename, detected_object)
+                            print(timestamp)
+                        else:
+                            print("already exists")
+
+                else:
+                    processed_file.add(filename)
+                    file_id = extract_id(filename)
+                    for i, detected_object in enumerate(VideoStream(link_constructor(filename)).rtd()):
+                        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                        filename = f'{output_folder}/{file_id}/{file_id}_{i}'
+                        if check_and_add_image(filename, 'ml_/models/sift/data/images') is True:
+                            cv2.imwrite(filename, detected_object)
+                            cv2.imwrite('ml_/models/sift/data/images'+filename, detected_object)
+                            print(timestamp)
+                        else:
+                            print("already exists")
 
 
-def detect(links:[]):
-    pass
+if __name__ == "main":
+    parser = argparse.ArgumentParser(description='Object detection script')
+    parser.add_argument('-i', '--input_folder', type=str, required=True, help='Input folder path')
+    parser.add_argument('-o', '--output_folder', type=str, required=True, help='Output folder path')
+    args = parser.parse_args()
+    check_folder(args.input_folder, args.output_folder)
