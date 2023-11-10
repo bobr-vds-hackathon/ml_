d3621a662cb1d1a006aeb6fe1995d1bdb9a3f01d
krell11 <11bobin.kiryusha@gmail.com>
2023-11-10 20:22:00 +0300

    skript changes

--- video/real_time_obj_detection.py
+++ video/real_time_obj_detection.py
@@ -1,52 +1,44 @@
 from video_stream import VideoStream
 import argparse
 import os
-from json_parser import parse_json, link_constructor, extract_id
 import datetime
 import cv2
 from models.sift.sift import check_and_add_image
+from json_parser import parse_json, link_constructor, extract_id
 
 
 def check_folder(input_folder, output_folder):
     processed_file = set()
     while True:
         for filename in os.listdir(input_folder):
-            if os.path.isfile(os.path.join(input_folder, filename)) and filename not in processed_file:
-                if filename.endswith('.json'):
-                    processed_file.add(filename)
-                    data = parse_json(filename)
-                    file_id = extract_id(filename)
-                    for i, detected_object in enumerate(VideoStream(link_constructor(data)).real_time_detection()):
+            filepath = os.path.join(input_folder, filename)
+            if os.path.isfile(filepath) and filename not in processed_file:
+                try:
+                    if filename.endswith('.json'):
+                        data = parse_json(filename)
+                        file_id = extract_id(filename)
+                        video_stream = VideoStream(link_constructor(data)).real_time_detection()
+                    else:
+                        file_id = extract_id(filename)
+                        video_stream = VideoStream(filepath).real_time_detection()
+
+                    for i, detected_object in enumerate(video_stream):
                         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                        filename_image = f'{output_folder}/{file_id}/{file_id}_{i}.jpeg'
-                        if check_and_add_image(filename_image, 'ml_/models/sift/data/images') is not False:
-                            cv2.imwrite(filename_image, detected_object)
-                            cv2.imwrite('ml_/models/sift/data/images/'+filename, detected_object)
-                            message = {"id": file_id,
-                                       "file": filename_image,
-                                       "timestamp": timestamp}
+                        image_folder = os.path.join(output_folder, file_id)
+                        if not os.path.exists(image_folder):
+                            os.makedirs(image_folder)
+                        image_filename = f'{image_folder}/{file_id}_{i}.jpeg'
+                        if check_and_add_image(image_filename, 'ml_/models/sift/data/images') is not False:
+                            cv2.imwrite(image_filename, detected_object)
+                            cv2.imwrite(os.path.join('ml_/models/sift/data/images', image_filename), detected_object)
+                            message = {"id": file_id, "file": image_filename, "timestamp": timestamp}
                             print(message)
-                        else:
-                            pass
-
-                else:
                     processed_file.add(filename)
-                    file_id = extract_id(filename)
-                    for i, detected_object in enumerate(VideoStream(filename).real_time_detection()):
-                        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                        filename_image = f'{output_folder}/{file_id}/{file_id}_{i}.jpeg'
-                        if check_and_add_image(filename, 'ml_/models/sift/data/images') is not False:
-                            cv2.imwrite(filename, detected_object)
-                            cv2.imwrite('ml_/models/sift/data/images/'+filename, detected_object)
-                            message = {"id": file_id,
-                                       "file": filename_image,
-                                       "timestamp": timestamp}
-                            print(message)
-                        else:
-                            pass
+                except Exception as e:
+                    print(f"Error processing {filename}: {e}")
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Object detection script')
     parser.add_argument('-i', '--input_folder', type=str, required=True, help='Input folder path')
     parser.add_argument('-o', '--output_folder', type=str, required=True, help='Output folder path')
